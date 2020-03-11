97b137a282e5f3088e868144f84ff9ec3e1658a5
Chenqiyuan <627064471@qq.com>
2020-03-11 10:58:57 +0800

    Initial Commit

---
+++ src/main/java/com/example/infect_statistic_web/InfectStatisticWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.infect_statistic_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfectStatisticWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfectStatisticWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infect_statistic_web/InfectStatisticWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infect_statistic_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfectStatisticWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
